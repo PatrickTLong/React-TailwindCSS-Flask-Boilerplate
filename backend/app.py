d8049a87004870b602fa6aa717717cd748e6ae14
Patrick Long <longpatrick3317@gmail.com>
2024-12-26 00:01:28 -0500

    fixed

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,25 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import sqlalchemy as sa
-import sqlalchemy.orm as so
 from dotenv import load_dotenv
 import os
+
+# Load environment variables
 load_dotenv()
+
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/dbname'
-#the above is for local development. 
-# Once you want to store the URL
-# in a safe location remove the above.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# Use DATABASE_URL from the environment, fallback to local development URI
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URL', 'postgresql://username:password@localhost:5432/dbname'
+)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 
 CORS(app)
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 
 @app.route("/")
 def start():
     return jsonify("Works")
-
-    
